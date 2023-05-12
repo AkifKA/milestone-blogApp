b5af39980545de52a7a6937544b08b8e179f2749
Mehmet Akif Karaöz <karaozmehmetakif@gmail.com>
2023-05-12 18:55:36 +0300

    package.json

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import AppRouter from "./router/AppRouter";
-import { grey, blueGrey } from "@mui/material/colors";
 import { Provider } from "react-redux";
-import store from "./app/store";
+import store, { persistor } from "./app/store";
+import { PersistGate } from "redux-persist/integration/react";
 import { ToastContainer } from "react-toastify";
 
 function App() {
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: grey["900"],
-      },
-      secondary: {
-        main: blueGrey["900"],
-      },
-    },
-  });
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <Provider store={store}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
           <AppRouter />
-        </Provider>
-        <ToastContainer />
-      </ThemeProvider>
+        </PersistGate>
+      </Provider>
+      <ToastContainer />
     </>
   );
 }
