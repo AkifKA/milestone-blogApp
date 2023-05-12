61bd90c7ce8330fa7e3214306a753845759e1ed9
Mehmet Akif Karaöz <karaozmehmetakif@gmail.com>
2023-05-12 18:49:54 +0300

    rooter

--- src/components/rooter/Approuter.jsx
+++ src/components/rooter/Approuter.jsx
@@ -1,22 +1,38 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import NewBlog from "../pages/NewBlog";
 import Dashboard from "../pages/Dashboard";
-import Register from "../pages/Register";
+import About from "../pages/About";
 import Login from "../pages/Login";
 import PrivateRouter from "./PrivateRouter";
+import NewBlog from "../pages/NewBlog";
+import Profile from "../pages/Profile";
+import MyBlog from "../pages/MyBlog";
+import Detail from "../pages/Detail";
+import Register from "../pages/Register";
+import NotFound from "../pages/NotFound";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const AppRouter = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="register" element={<Register />} />
+        <Route path="about" element={<About />} />
         <Route path="login" element={<Login />} />
-        <Route path="new-block" element={<PrivateRouter />}>
-          <Route index element={<NewBlog />} />
+        <Route path="logout" element={<Dashboard />} />
+        <Route path="register" element={<Register />} />
+        <Route path="" element={<PrivateRouter />}>
+          <Route path="my-blogs/detail/:id" element={<Detail />} />
+          <Route path="new-blog" element={<NewBlog />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="my-blogs" element={<MyBlog />} />
+          <Route path="detail/:id" element={<Detail />} />
         </Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
+      <Footer />
     </Router>
   );
 };
